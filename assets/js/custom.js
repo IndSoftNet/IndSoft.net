7b1232e9a6be6e3229335c7aa7e05a0370f5bc3e
AnilChinchawale <anil24593@gmail.com>
2019-10-14 10:35:48 +0530

    Official Cloud IndSoft Site

---
+++ assets/js/custom.js
@@ -0,0 +1,99 @@
+(function($){
+    "use strict"
+    var INDSOFT = {};
+
+    /*--------------------
+        * Header Class
+    ----------------------*/
+    INDSOFT.HeaderSticky = function(){
+        $(".navbar-toggler").on("click", function(a) {
+            a.preventDefault(), 
+            $(".navbar").addClass("fixed-header")
+        });
+    }
+
+    /*--------------------
+        * Menu Close
+    ----------------------*/
+    INDSOFT.MenuClose = function(){
+      $('.navbar-nav .nav-link').on('click', function() {
+       var toggle = $('.navbar-toggler').is(':visible');
+       if (toggle) {
+         $('.navbar-collapse').collapse('hide');
+       }
+      });
+    }
+
+
+    /*--------------------
+        * Smooth Scroll
+    ----------------------*/
+    INDSOFT.HeaderScroll = function(){
+        $('header a[href*="#"]:not([href="#"])').on('click', function() {
+          var PathName = location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') || location.hostname == this.hostname;
+            if (PathName) {
+              var target = $(this.hash);
+                  target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+                  if (target.length) {
+                    $('html,body').animate({
+                      scrollTop: target.offset().top - 65,
+                      }, 1000);
+                      return false;
+                  }
+            }
+        });
+    }
+
+
+    /*--------------------
+        * Footer Smooth Scroll
+    ----------------------*/
+    INDSOFT.FooterScroll = function(){
+        $('.fot-link a[href*="#"]:not([href="#"])').on('click', function() {
+          var PathName = location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') || location.hostname == this.hostname;
+            if (PathName) {
+              var target = $(this.hash);
+                  target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+                  if (target.length) {
+                    $('html,body').animate({
+                      scrollTop: target.offset().top - 65,
+                      }, 1000);
+                      return false;
+                  }
+            }
+        });
+    }
+	
+	
+    /*--------------------
+        * Header Fixed
+    ----------------------*/
+    INDSOFT.HeaderFixed = function(){
+      var varHeaderFix = $(window).scrollTop() >= 60;
+        if (varHeaderFix) {
+           $('.navbar').addClass('fixed-header');
+        }
+        else {
+           $('.navbar').removeClass('fixed-header');
+        }
+    }    
+
+    
+
+    // Window on Load
+    $(window).on("load", function(){
+      INDSOFT.WebLoad();
+    });
+
+    $(document).ready(function() {
+        INDSOFT.MenuClose(),
+        INDSOFT.HeaderScroll(),
+		INDSOFT.FooterScroll(),
+        INDSOFT.HeaderSticky();
+    });
+
+    $(window).on("scroll", function(){
+        INDSOFT.HeaderFixed();
+    });
+
+})(jQuery);

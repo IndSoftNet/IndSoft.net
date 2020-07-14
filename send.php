7e9b556f107c5853063061c15669ba78aaf02bad
AnilChinchawale <anil24593@gmail.com>
2020-07-14 16:14:01 +0530

    update pages

--- send.php
+++ send.php
@@ -1,73 +1,94 @@
 <?php
-require 'mail/PHPMailerAutoload.php';
+// require 'mail/PHPMailerAutoload.php';
 //require 'class.pop3.php';
 //require_once 'class.smtp.php';
 //include('Mail.php');
-include('pear.php');
+// include('pear.php');
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+require 'vendor/autoload.php';
 
   //declare our variables
-  $name = $_POST['your-name'];
-  $email = $_POST['your-email'];
-  $telephone = $_POST['your-phone'];
-  $company_name = $_POST['company-name'];
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+  $telephone = $_POST['phone'];
+  $company_name = $_POST['company'];
   $website = $_POST['website'];
   $pageviews = $_POST['pageviews'];
   $country = $_POST['country'];
-  $message = $_POST['your-message'];
+  $message = $_POST['message'];
 
  $to = 'support@indsoft.net,mktg@indsoft.net'; 
  $from = $email;
  $subject = "Contact form filled on Cloud.IndSoft.net";
- $body = "Hello $name,<br>
+ $body = "Hello $name,
   We received an email request!
-  <br><br><br><br>
-  Name :- $name<br><br>
-  E-mail :- $email<br><br>
-  Mobile Number :- $telephone<br><br>
-  Company :- $company_name<br><br>
-  Website :- $website<br><br>
-  Pageviews :- $pageviews<br><br>
-  Country :- $country<br><br>
-  Message :- $message<br>";
+  Name :- $name
+  E-mail :- $email
+  Mobile Number :- $telephone
+  Company :- $company_name
+  Website :- $website
+  Pageviews :- $pageviews
+  Country :- $country
+  Message :- $message";
 
- $host = "mail-b01.cloudmailbox.in";
- $smtpinfo["auth"] = true;
- $smtpinfo["username"] = "indsoft@mail-b01.cloudmailbox.in";
- $smtpinfo["password"] = "Ind@mb01#$039";
+ //TLS Port 587 Change if not TLS
+ $mail = new PHPMailer;
+ $mail->isSMTP();
+ $mail->Host = 'mail-b01.cloudmailbox.in';
+ $mail->Port = 587;
+ $mail->SMTPAuth = true;
+ $mail->Username = 'indsoft@mail-b01.cloudmailbox.in';
+ $mail->Password = 'Ind@mb01#$039';
+ $mail->setFrom($email, $name);
+ $mail->addAddress(' support@indsoft.net','mktg@indsoft.net','ssarveshchauhan@gmail.com');
+ $mail->Subject = $subject;
+ $mail->isHTML(false);
+ $mail->Body = $body;
 
- $headers = array ('From' => $from,'To' => $to,'Subject' => $subject);
+ 
+//  $host = "mail-b01.cloudmailbox.in";
+//  $smtpinfo["auth"] = true;
+//  $smtpinfo["username"] = "indsoft@mail-b01.cloudmailbox.in";
+//  $smtpinfo["password"] = "Ind@mb01#$039";
+
+
+//  $headers = array ('From' => $from,'To' => $to,'Subject' => $subject);
  //$smtp = mail::factory('smtp',array ('host' => $host,'auth' => true,'username' => $username,'password' => $password));
 
- $mail = new PHPMailer();
+//  $mail = new PHPMailer();
 
-	$mail->IsSMTP(); // telling the class to use SMTP
-	$mail->IsHTML(true);
-	$mail->Host       = "mail-b01.cloudmailbox.in"; // SMTP server
+// 	$mail->IsSMTP(); // telling the class to use SMTP
+// 	$mail->IsHTML(true);
+// 	$mail->Host       = "mail-b01.cloudmailbox.in"; // SMTP server
   	
-	$mail->From       = $from;
-	$mail->FromName   = $from;
-	$mail->Subject    = $subject;
+// 	$mail->From       = $from;
+// 	$mail->FromName   = $from;
+// 	$mail->Subject    = $subject;
 
-	$mail->MsgHTML($body);
+// 	$mail->MsgHTML($body);
 	
-	$mail->AddReplyTo ($email);
+// 	$mail->AddReplyTo ($email);
 
-  $mail->AddAddress($to, $to);
+//   $mail->AddAddress($to, $to);
   
-  $mail-> AddBCC ("anil@indsoft.net");
+//   $mail-> AddBCC ("anil@indsoft.net");
 
 	//$mail->Send();
 
- 	$mail->send($to, $headers, $body);
+ 	// $mail->send($to, $headers, $body);
 
  //$mail->send($to, $headers, $body);
  // $mail = $smtp->send($to, $headers, $body);
 
-  if (pear::isError($mail))
-   {echo("<p>" . $mail->getMessage() . "</p>");}
-  else
-   {
-    echo("<p>Message successfully sent!</p>");
-    header("Location:https://Cloud.IndSoft.net/");
-
+  if (!$mail->send()){
+    echo  "Fail to sent messagee";
   }
+  else{
+    echo "Message successfully sent!";
+    // header("Location:https://Cloud.IndSoft.net/");
+  }
+  ?>
